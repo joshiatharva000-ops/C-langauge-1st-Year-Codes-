6113422e13a27d83996f54bbf07687bb5efb007b
Atharv Joshi <joshiatharva000@gmail.com>
2025-09-30 23:54:19 +0530

    Add files via upload

---
+++ abc.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int side;
+    printf("Enter the side of the square: ");
+    scanf("%d", &side);
+    printf("Area of square is %d", side*side);
+    return 0;
+}
