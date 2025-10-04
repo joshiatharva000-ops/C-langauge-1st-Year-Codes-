1a7ca4008594925c97947b66a1fc48d66d0d77bb
Atharv Joshi <joshiatharva000@gmail.com>
2025-10-04 22:46:43 +0530

    Add files via upload

---
+++ aaa.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter a: ");
+        scanf("%d",&a);
+    printf("enter b: ");
+        scanf("%d",&b);
+    for (int i=a;i<=b;i++){
+        printf("%d\n",i);
+        
+    }
+    return 0;
+}
