94104cb9adbc65d3dc5fda89537082a180a6018f
Atharv Joshi <joshiatharva000@gmail.com>
2025-10-06 23:27:09 +0530

    Add files via upload

---
+++ bcbc.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    printf("Numbers from 1 to 1000000:\n");
+
+    for(i = 1; i <= 1000000; i++) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
